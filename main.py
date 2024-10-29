ab110d377cbf90db4f8e9f15f8da373701608ebf
Sohom <sohom.ban96@gmail.com>
2024-10-29 22:15:11 +0530

    Data validation added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
-# from NetworkSecurity.components.data_validation import DataValidation
+from NetworkSecurity.components.data_validation import DataValidation
 # from NetworkSecurity.components.data_transformation import DataTransformation
 from NetworkSecurity.exceptions.exceptions import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
@@ -21,5 +21,12 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
