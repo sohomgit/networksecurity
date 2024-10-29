3da370d888506dc537ba7d3aad6c9319dc92894c
Sohom <sohom.ban96@gmail.com>
2024-10-29 17:40:47 +0530

    ETL pipeline added

--- NetworkSecurity/logging/logger.py
+++ NetworkSecurity/logging/logger.py
@@ -2,14 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-log_file = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
-log_path = os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs(log_path,exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_file_path = os.path.join(log_path,log_file)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename= log_file_path,
-    format= "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO(),
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
